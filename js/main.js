6bc9e95b04a7a7465b35a7147534f67ecc29a88a
muhmmad Nabh <muhammadnabih2020@gmail.com>
2023-09-25 14:17:13 +0300

    Create Function which Calculate Total Price

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-let title = document.querySelector("#title");
-let price = document.querySelector("#price");
-let taxes = document.querySelector("#taxes");
-let ads = document.querySelector("#ads");
-let discount = document.querySelector("#discount");
-let count = document.querySelector("#count");
-let total = document.querySelector("#total");
-let category = document.querySelector("#category");
-let submit = document.querySelector("#submit");
-let btnDelete = document.getElementById("deleteAll");
+// CALL ALL ELEMENTS
+const priceInputs = document.querySelectorAll('.price input[type="number"]');
+const totalElement = document.querySelector("small");
+
+priceInputs.forEach((input) => {
+  input.addEventListener("input", calculateTotalPrice);
+});
+
+// <== START EVENT HANDLERS==>
+function calculateTotalPrice() {
+  // استخراج القيم من عناصر الإدخال وتحويلها إلى أرقام عائمة
+  const priceValue = parseFloat(priceInputs[0].value || 0); // قيمة Price
+  const taxesValue = parseFloat(priceInputs[1].value || 0); // قيمة Taxes
+  const adsValue = parseFloat(priceInputs[2].value || 0); // قيمة Ads
+  const discountValue = parseFloat(priceInputs[3].value || 0); // قيمة Discount
+  if (priceValue > 0) {
+    const totalPrice = priceValue + taxesValue + adsValue - discountValue;
+    totalElement.textContent = `Total: ${totalPrice.toFixed(2)}`;
+    totalElement.style.background = "#ff8906";
+  } else {
+    totalElement.style.background = "transparent";
+    totalElement.textContent = "";
+  }
+}
+// <== END EVENT HANDLERS ==>
